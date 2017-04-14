17ac44ed5bc44c82b9319c784b6c7aff43bdfcad
dheeraj444 <drj1033@gmail.com>
2017-04-14 11:20:50 -0500

    updated docker-install.rb

--- cookbooks/ec2-docker/recipes/docker-install.rb
+++ cookbooks/ec2-docker/recipes/docker-install.rb
@@ -12,16 +12,11 @@ end
 execute 'mkdir' do
   cwd '/etc/systemd/system'
   command 'mkdir docker.service.d'
- end
-
-execute 'touch' do
-  cwd '/etc/systemd/system/docker.service.d/'
-  command 'touch docker.conf'
 end
 
 file '/etc/systemd/system/docker.service.d/docker.conf' do
   content '[Service]
    ExecStart=
-   ExecStart=/usr/bin/dockerd -H tcp://172.31.68.24:2376'
+   ExecStart=/usr/bin/dockerd -H tcp://{USE OHAI HOSTNAME/ IP ADDRESS}:2376'
 end
 
