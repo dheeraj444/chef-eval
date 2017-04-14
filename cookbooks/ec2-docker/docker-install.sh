6d166f7456828e32f983f1d61e323a739bb7e48f
dheeraj444 <drj1033@gmail.com>
2017-04-14 11:18:41 -0500

    added docker-install.sh

---
+++ cookbooks/ec2-docker/docker-install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+yum update -y
+yum install -y yum-utils
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum makecache fast
+yum install docker-ce -y
